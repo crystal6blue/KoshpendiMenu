04596491e1837bce3e5604ff57a223f0fed3b04d
bnurd <b.nurdaulet2626@gmail.com>
2025-05-03 22:26:10 +0500

    Init

---
+++ src/main/java/com/project/koshpendimenu1/KoshpendiMenu1Application.java
@@ -0,0 +1,13 @@
+package com.project.koshpendimenu1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoshpendiMenu1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoshpendiMenu1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/koshpendimenu1/KoshpendiMenu1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.koshpendimenu1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoshpendiMenu1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
